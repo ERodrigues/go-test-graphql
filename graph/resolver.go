71e980e39c59d61e182338f3c90ae63b0e14ce77
Eder Rodrigues <ederpr@gmail.com>
2023-02-07 06:44:43 -0300

    remocao de dependencia indevida

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,7 +2,6 @@ package graph
 
 import (
 	"github.com/ERodrigues/go-test-graphql/graph/internal/database"
-	"google.golang.org/genproto/googleapis/spanner/admin/database/v1"
 )
 
 // This file will not be regenerated automatically.
