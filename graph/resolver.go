2a59868fe20c764ac707800a79d325f9c1b71307
Eder Rodrigues <ederpr@gmail.com>
2023-02-07 06:24:51 -0300

    injetando a dependencia do objeto Category

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,14 @@
 package graph
 
+import (
+	"github.com/ERodrigues/go-test-graphql/graph/internal/database"
+	"google.golang.org/genproto/googleapis/spanner/admin/database/v1"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	CategoryDB *database.Category
+}
